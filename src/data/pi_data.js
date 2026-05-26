b356896bcc6da0a0f9e9d1f552f4ea5098dea645
EVE Planner <planner@eve.local>
2026-05-25 21:40:07 -0400

    data: Update planet-to-resource mapping to match requested distribution

--- src/data/pi_data.js
+++ src/data/pi_data.js
@@ -135,16 +135,16 @@ export const commodities = [
 ];
 
 export const RESOURCE_TO_PLANETS = {
-    2268: ["Barren", "Gas", "Ice", "Oceanic", "Storm", "Temperate"], // Aqueous Liquids
+    2268: ["Gas", "Ice", "Oceanic", "Storm", "Temperate"], // Aqueous Liquids
     2305: ["Temperate"], // Autotrophs
     2267: ["Barren", "Gas", "Lava", "Plasma", "Storm"], // Base Metals
     2288: ["Barren", "Oceanic", "Temperate"], // Carbon Compounds
     2287: ["Oceanic", "Temperate"], // Complex Organisms
     2307: ["Lava"], // Felsic Magma
     2272: ["Lava", "Plasma"], // Heavy Metals
-    2309: ["Gas", "Storm"], // Ionic Solutions
+    2309: ["Storm"], // Ionic Solutions
     2073: ["Barren", "Ice", "Oceanic", "Temperate"], // Microorganisms
-    2310: ["Gas"], // Noble Gas
+    2310: [], // Noble Gas
     2270: ["Barren", "Gas", "Plasma", "Storm"], // Noble Metals
     2306: ["Lava", "Plasma"], // Non-CS Crystals
     2286: ["Ice", "Oceanic"], // Planktic Colonies
@@ -155,8 +155,8 @@ export const RESOURCE_TO_PLANETS = {
 };
 
 export const PLANET_RESOURCES = {
-    Barren: [2268, 2267, 2288, 2073, 2270],
-    Gas: [2268, 2267, 2310, 2309, 2270, 2311],
+    Barren: [2267, 2288, 2073, 2270],
+    Gas: [2268, 2267, 2270, 2311],
     Ice: [2268, 16272, 16273, 2073, 2286],
     Lava: [2267, 2307, 2272, 2306, 2308],
     Oceanic: [2268, 2288, 2073, 2286, 2287],
